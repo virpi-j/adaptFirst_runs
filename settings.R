35922eedc879eee90fb8ffa7302feb461ca538ef
Virpi Junttila <vjunttil@r07c51.bullx>
2023-02-28 11:57:05 +0200

    update

--- settings.R
+++ settings.R
@@ -33,7 +33,7 @@ if(CSCrun){
   libpath <- .libPaths()[1]
 }
 ###choose PREBAS version
-if(!exists("vPREBAS")) vPREBAS <- "v1.0.0" #"master"   #### choose PREBAS version to run the model  "master" "v0.2.x"
+if(!exists("vPREBAS")) vPREBAS <- "master" #"v1.0.0" #"master"   #### choose PREBAS version to run the model  "master" "v0.2.x"
 devtools::install_github("ForModLabUHel/Rprebasso", ref=vPREBAS)
 
 library(Rprebasso)
