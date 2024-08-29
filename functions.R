a75d4499b95ca59b3a8e42e564f5be8c7dd9238d
Virpi Junttila <vjunttil@r07c51.bullx>
2024-08-29 14:37:38 +0300

    update for testing

--- functions.R
+++ functions.R
@@ -472,7 +472,8 @@ runModelAdapt <- function(deltaID,sampleID=1, climScen=0, outType="dTabs",rcps =
         region <- regionPrebas(initPrebas, HarvLim = as.numeric(HarvLimX),
                                cutAreas =cutArX,compHarv=compHarvX)
       } else {
-        #save(initPrebas, HarvLimX, minDharvX,cutArX,compHarvX, file="testRegionInput.rdata")
+        save(initPrebas, HarvLimX, minDharvX,cutArX,compHarvX, 
+             file=paste0("testRegionInput_",station_id,".rdata"))
         region <- regionPrebas(initPrebas, HarvLim = as.numeric(HarvLimX),
                                minDharv = minDharvX,cutAreas =cutArX,
                                compHarv=compHarvX)
